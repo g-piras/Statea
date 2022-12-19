ac09c0c66527fe4a322975f2492cf9c1476b3d8f
KelosDev <gpiraswebdev@gmail.com>
2022-12-19 23:04:52 +0100

    [style] homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,8 +16,8 @@ const Navbar = () => {
     }
 
     return (
-        <div className='fixed w-full h-20 shadow-xl z-[100]'>
-            <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+        <div className='fixed w-full h-20 shadow-xl z-[100] backdrop-filter backdrop-blur-lg '>
+            <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16 absolute'>
                 <Image src="/../public/assets/touristats-logo.png" alt="/" width='70' height='70' />
                 <div>
                     <ul className='hidden md:flex'>
@@ -37,9 +37,9 @@ const Navbar = () => {
                 </div>
             </div>
 
-            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70 ' : ''}>
                 <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' :
-                    'fixed left-[-100%] top-0  p-10 ease-in duration-500'}>
+                    'fixed left-[-100%] top-0  p-10 ease-in duration-500 '}>
                     <div>
                         <div className='flex w-full items-center justify-between'>
                             <Image src="/../public/assets/touristats-logo.png" alt="/" width='70' height='70' />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+
+        'mobile': '480px',
+        // => @media (min-width: 480px) { ... }
+
+        'tablet': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'laptop': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'desktop': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 }

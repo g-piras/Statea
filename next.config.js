8bcd415748d5848aeb23b9618ceaf4359912745c
Elanigiro <emilio.gasbarro@outlook.com>
2023-01-11 09:39:10 +0100

    added docker config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  unoptimized: true
-}
-
-images: {
-  unoptimized: true
-}
-
-module.exports = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  nextConfig
+  reactStrictMode: true,
+  swcMinify: true,
+  unoptimized: true,
+  output: 'standalone',
 }
+
+module.exports = nextConfig

55145a15232dbd908ba345d46e8e62905aca8af2
KelosDev <gpiraswebdev@gmail.com>
2023-01-12 07:12:29 +0100

    [feat] add photo teammates on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -50,10 +50,10 @@ const about = () => {
         <section className="w-full h-full mb-[160px]">
           <div className=" lg:pt-36 relative">
             <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-10 row-gap-8 mx-auto sm:row-gap-10 lg:max-w-screen-lg lg:grid-cols-3">
-
+              {/* div only for mobile */}
               <div className="relative lg:top-[-100px] transition transform delay-150 lg:hover:scale-150 lg:hidden md:hidden sm:block ">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow " src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow " src="/assets/Joele.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Joele Melchiorre</p>
                     <p className="text-sm text-gray-800 text-center">Front-End Developer <br />Team Leader</p>
@@ -63,7 +63,7 @@ const about = () => {
 
               <div className="relative lg:left-[39px] transition transform delay-150 lg:hover:scale-150 ">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow" src="/assets/sebastiano.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Sebastiano Demichelis</p>
                     <p className="text-sm text-gray-800 text-center">Front-End Developer</p>
@@ -73,7 +73,7 @@ const about = () => {
 
               <div className="relative lg:top-[-100px] transition transform delay-150 lg:hover:scale-150 hidden lg:block md:block">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow " src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow " src="/assets/Joele.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Joele Melchiorre</p>
                     <p className="text-sm text-gray-800 text-center">Front-End Developer <br />Team Leader</p>
@@ -83,7 +83,7 @@ const about = () => {
 
               <div className="relative transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow" src="/assets/Federico.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Federico Luciano Stroppiana</p>
                     <p className="text-sm text-gray-800 text-center">Front-End Developer</p>
@@ -93,7 +93,7 @@ const about = () => {
 
               <div className="relative flex flex-col justify-center lg:w-[150px] transition transform delay-150 lg:hover:scale-150 ">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow" src="/assets/giampietro.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Giampietro Piras</p>
                     <p className="text-sm text-gray-800 text-center">Front-End Developer</p>
@@ -107,7 +107,7 @@ const about = () => {
 
               <div className="relative flex flex-col justify-center lg:w-[150px] lg:left-[159px] transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center ">
-                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow" src="/assets/Emilio.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Emilio Gasbarro</p>
                     <p className="text-sm text-gray-800 text-center">Back-End Developer</p>
@@ -117,7 +117,7 @@ const about = () => {
 
               <div className="relative transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow" src="/assets/matteo2.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Matteo Taricco</p>
                     <p className="text-sm text-gray-800 text-center">Back-End Developer</p>
@@ -127,7 +127,7 @@ const about = () => {
 
               <div className="relative lg:bottom-[-100px] transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20  rounded-full shadow" src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28  rounded-full shadow" src="/assets/fabio.png" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold">Fabio Leoni</p>
                     <p className="text-sm text-gray-800">Fintech Developer</p>
@@ -137,7 +137,7 @@ const about = () => {
 
               <div className="relative transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <img className="object-cover w-28 h-28 rounded-full shadow" src="/assets/Matteo.jpg" alt="Person" />
                   <div className="flex flex-col justify-center">
                     <p className="text-lg font-bold ">Matteo Drago</p>
                     <p className="text-sm text-gray-800 text-center">Fintech Developer</p>

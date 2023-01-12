653a0d0fd224eaa3abff1d28435737da21e5f08c
KelosDev <gpiraswebdev@gmail.com>
2023-01-12 06:42:09 +0100

    [fix] team leader icon was not at the top in mobile view

--- pages/about.tsx
+++ pages/about.tsx
@@ -36,7 +36,7 @@ const about = () => {
           <h3 className="uppercase text-gray-800 mt-[100px] mb-2 px-10 text-2xl">
             con il nostro prodotto
           </h3>
-          <p className="tracking-wide text-gray-600 mx-auto max-w-[50%] pt-10 text-lg ">
+          <p className="tracking-wide text-gray-600 mx-auto max-w-[60%] pt-10 text-lg ">
             ci poniamo l'obiettivo di <span className="font-bold uppercase">aiutare</span> le aziende del settore turistico a <span className="font-bold uppercase">comprendere</span> e
             sfruttare al meglio i dati e le previsioni presenti all'interno di <span className="text-[#284697] text-xl font-bold">Touri</span>
             <span className="text-[#00ACC1] text-xl font-bold">Stats</span> per migliorare il loro <span className="font-bold uppercase">business</span>.
@@ -51,17 +51,7 @@ const about = () => {
           <div className=" lg:pt-36 relative">
             <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-10 row-gap-8 mx-auto sm:row-gap-10 lg:max-w-screen-lg lg:grid-cols-3">
 
-              <div className="relative lg:left-[39px] transition transform delay-150 hover:scale-150">
-                <div className="flex flex-col items-center">
-                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
-                  <div className="flex flex-col justify-center">
-                    <p className="text-lg font-bold ">Sebastiano Demichelis</p>
-                    <p className="text-sm text-gray-800 text-center">Front-End Developer</p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="relative lg:top-[-100px] transition transform delay-150 hover:scale-150">
+              <div className="relative lg:top-[-100px] transition transform delay-150 lg:hover:scale-150 lg:hidden md:hidden sm:block ">
                 <div className="flex flex-col items-center">
                   <img className="object-cover w-20 h-20 rounded-full shadow " src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
                   <div className="flex flex-col justify-center">
@@ -71,7 +61,27 @@ const about = () => {
                 </div>
               </div>
 
-              <div className="relative transition transform delay-150 hover:scale-150">
+              <div className="relative lg:left-[39px] transition transform delay-150 lg:hover:scale-150 ">
+                <div className="flex flex-col items-center">
+                  <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <div className="flex flex-col justify-center">
+                    <p className="text-lg font-bold ">Sebastiano Demichelis</p>
+                    <p className="text-sm text-gray-800 text-center">Front-End Developer</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="relative lg:top-[-100px] transition transform delay-150 lg:hover:scale-150 hidden lg:block md:block">
+                <div className="flex flex-col items-center">
+                  <img className="object-cover w-20 h-20 rounded-full shadow " src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+                  <div className="flex flex-col justify-center">
+                    <p className="text-lg font-bold ">Joele Melchiorre</p>
+                    <p className="text-sm text-gray-800 text-center">Front-End Developer <br />Team Leader</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="relative transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
                   <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
                   <div className="flex flex-col justify-center">
@@ -81,7 +91,7 @@ const about = () => {
                 </div>
               </div>
 
-              <div className="relative flex flex-col justify-center lg:w-[150px] transition transform delay-150 hover:scale-150 ">
+              <div className="relative flex flex-col justify-center lg:w-[150px] transition transform delay-150 lg:hover:scale-150 ">
                 <div className="flex flex-col items-center">
                   <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
                   <div className="flex flex-col justify-center">
@@ -91,11 +101,11 @@ const about = () => {
                 </div>
               </div>
 
-              <div className="lg:flex lg:justify-center hidden hover:animate-rotate-360 ">
+              <div className="lg:flex lg:justify-center hidden lg:hover:animate-rotate-360 ">
                 <img className="object-cover w-150 h-150  " src="/assets/statea2.png" alt="Logo" />
               </div>
 
-              <div className="relative flex flex-col justify-center lg:w-[150px] lg:left-[159px] transition transform delay-150 hover:scale-150">
+              <div className="relative flex flex-col justify-center lg:w-[150px] lg:left-[159px] transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center ">
                   <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
                   <div className="flex flex-col justify-center">
@@ -105,7 +115,7 @@ const about = () => {
                 </div>
               </div>
 
-              <div className="relative transition transform delay-150 hover:scale-150">
+              <div className="relative transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
                   <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
                   <div className="flex flex-col justify-center">
@@ -115,7 +125,7 @@ const about = () => {
                 </div>
               </div>
 
-              <div className="relative lg:bottom-[-100px] transition transform delay-150 hover:scale-150">
+              <div className="relative lg:bottom-[-100px] transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
                   <img className="object-cover w-20 h-20  rounded-full shadow" src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
                   <div className="flex flex-col justify-center">
@@ -125,7 +135,7 @@ const about = () => {
                 </div>
               </div>
 
-              <div className="relative transition transform delay-150 hover:scale-150">
+              <div className="relative transition transform delay-150 lg:hover:scale-150">
                 <div className="flex flex-col items-center">
                   <img className="object-cover w-20 h-20 rounded-full shadow" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
                   <div className="flex flex-col justify-center">

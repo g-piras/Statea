0db57c7f93771b7c74b58f6c9d352ab2ac4d8eb9
KelosDev <gpiraswebdev@gmail.com>
2023-01-16 01:00:49 +0100

    trying webpack compression

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const CompressionPlugin = require('compression-webpack-plugin');
+
 const nextConfig = {
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -11,4 +14,23 @@ const nextConfig = {
   output: 'standalone',
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config
+  },
+
+  plugins: [
+    new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.(js|css|html|svg)$/,
+      threshold: 8192,
+      minRatio: 0.8
+    })
+  ]
+}
